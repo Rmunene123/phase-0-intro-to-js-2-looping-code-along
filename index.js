bffeae59f17148453c3f202fef9eda3015fe5937
Rmunene123 <rosewanmun@gmail.com>
2024-07-02 16:25:11 +0300

    added looping lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
+module.exports = {
+    writeCards,
+    countDown
+  };
+  
